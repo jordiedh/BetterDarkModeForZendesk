1de3d0c7dd91cd565de2fa1b1cd875be93ee8bcf
JordieDH <orrotmg@gmail.com>
2025-05-16 09:26:33 +0930

    Fixed email formatting and buttons

--- content.js
+++ content.js
@@ -30,14 +30,14 @@ if (window.location.hostname.includes("zendesk.com")) {
 
 if (window.location.hostname.includes("zdusercontent.com")) {
     document.documentElement.style.backgroundColor = "#151a1e";
-    const style = document.createElement('style');
-    style.textContent = `[class*="MiniDisplayField"][class*="label"] {
+    const appStyle = document.createElement('style');
+    appStyle.textContent = `[class*="MiniDisplayField"][class*="label"] {
         color: #ffffff !important;
     }`;
+    document.head.appendChild(appStyle);
 
-    document.head.appendChild(style);
-    const htmlStyle = document.createElement('style');
-    htmlStyle.textContent = `html {
+    const appStyle2 = document.createElement('style');
+    appStyle2.textContent = `html {
         color: #d3d3d3 !important;
     }
     .bYrRLL {
@@ -49,6 +49,42 @@ if (window.location.hostname.includes("zdusercontent.com")) {
     .hTKgDl {
         color: #ffffff !important;
     }`;
-
-    document.head.appendChild(htmlStyle);
+    document.head.appendChild(appStyle2);
 }
+
+// If the text is written from email in black, change the text to white
+
+const commentStyle = document.createElement('style');
+commentStyle.textContent = `.zd-comment * {
+    color: inherit !important;
+    background-color: inherit !important;
+}
+
+    .zd-comment *[style*="color: #000"],
+    .zd-comment *[style*="color:#000"],
+    .zd-comment *[style*="color: rgb(0, 0, 0)"],
+    .zd-comment *[style*="color:rgb(0,0,0)"] {
+    color: rgb(216, 220, 222) !important;
+
+    .zd-comment *[style*="background-color: #fff"],
+    .zd-comment *[style*="background-color:#fff"],
+    .zd-comment *[style*="background-color: white"],
+    .zd-comment *[style*="background-color:white"],
+    zd-comment *[style*="background-color: rgb(255, 255, 255)"],
+    .zd-comment *[style*="background-color:rgb(255,255,255)"] {
+        background-color: transparent !important;
+    }
+}`;
+document.head.appendChild(commentStyle);
+
+// Change the button formatting
+const buttonStyle = document.createElement('style');
+buttonStyle.textContent = `div[role="button"][disabled] {
+    background-color: #344353 !important;
+    color: #8c9bb3 !important;
+}
+    
+div[role="button"]:not([disabled]) {
+    color: #fff !important;
+}`;
+document.head.appendChild(buttonStyle);
