76ebc03c0fa8c08c956afeaa66e97e1920bcbfa8
JordieDH <orrotmg@gmail.com>
2025-05-15 15:10:04 +0930

    Fix colours of apps

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+// Wait for the element to be available in the DOM
+
 const waitForElement = (selector, callback) => {
     const el = document.querySelector(selector);
     if (el) return callback(el);
@@ -16,8 +18,37 @@ const waitForElement = (selector, callback) => {
     });
 };
 
+// Update the navigation bar background color
+
 if (window.location.hostname.includes("zendesk.com")) {
     waitForElement('.eLDJvb', el => {
         el.style.backgroundColor = '#101112';
     });
+}
+
+// Update the background color and text colour of the apps
+
+if (window.location.hostname.includes("zdusercontent.com")) {
+    document.documentElement.style.backgroundColor = "#151a1e";
+    const style = document.createElement('style');
+    style.textContent = `[class*="MiniDisplayField"][class*="label"] {
+        color: #ffffff !important;
+    }`;
+
+    document.head.appendChild(style);
+    const htmlStyle = document.createElement('style');
+    htmlStyle.textContent = `html {
+        color: #d3d3d3 !important;
+    }
+    .bYrRLL {
+        color: #d3d3d3 !important;
+    }
+    .ciAvon {
+        color: #d3d3d3 !important;
+    }
+    .hTKgDl {
+        color: #ffffff !important;
+    }`;
+
+    document.head.appendChild(htmlStyle);
 }
